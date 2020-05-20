d64827441fad889d8f1ca73c57d9548faabd830e
ji <jkim0107@gmail.com>
2020-05-20 10:56:25 -0700

    working part1a solution

---
+++ src/node_part1a_1.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const contents = process.argv.splice(2);
+const data = JSON.parse(fs.readFileSync(contents[0], 'utf-8'));
+
+let currentCount = 0
+let currentTables = 0
+let parties = []
+let tables = []
+data['parties'].forEach(party => {
+  currentCount = party.count
+  while (currentCount > 0) {
+    parties.push(party.partyName)
+    currentCount--
+  }
+})
+
+data['tables'].forEach(table => {
+  currentTables = table.numberOfSeats
+  while (currentTables > 0) {
+    tables.push(table.tableName)
+    currentTables--
+  }
+})
+
+for (let i = 0; i < parties.length; i++) {
+  console.log(`${parties[i]} ${tables[i]}`)
+}
+
+// console.log('data =', data);
