1dac3bd2e31fe8f2e14632c5882d5053e0a1aa21
ji <jkim0107@gmail.com>
2020-05-20 12:03:32 -0700

    updated solution

--- src/node_solution.js
+++ src/node_solution.js
@@ -12,14 +12,15 @@ const seatsCount = data.tables.reduce((tot, curr) => {
   return curr.numberOfSeats + tot
 }, 0)
 
-let deductSeat = function(tableName, count) {
-  let table = data.tables.filter(t => {
+let deductSeat = function(tables, tableName, count) {
+  let table = tables.filter(t => {
     return t.tableName === tableName
   })
   if (count > table.numberOfSeats) {
     return false
   } else {
     table.numberOfSeats -= count
+    return tables
   }
 }
 
@@ -28,18 +29,19 @@ let tableIndex = 0
 if (partiesCount > seatsCount) {
   console.log(`We do not have enough seats!`)
 } else {
+  let unseedableTables = {}
   for (let i = 0; i < data.parties.length; i++) {
     let party = data.parties[i]
     if (party.hasOwnProperty('tableSeed')) {
-      if (!deductSeat(party.tableSeed, party.count)) {
-        console.log(`${party.tableSeed} does not have enough seats!`)
+      if (!deductSeat(data.tables, party.tableSeed, party.count)) {
+        unseedableTables[party.tableSeed] = party.tableSeed
       } else {
         while (party.count > 0) {
           console.log(`${party.partyName} at ${party.tableSeed}`)
           party.count--
         }
       }
-      deductSeat(party.tableSeed, party.count)
+      party.tables = deductSeat(data.tables, party.tableSeed, party.count)
     } else {
       if (party.count === 1) {
         i++
@@ -53,4 +55,7 @@ if (partiesCount > seatsCount) {
       data.tables[tableIndex].numberOfSeats--
     }
   }
+  Object.keys(unseedableTables).forEach(table => {
+    console.log(`${table} does not have enough seats!`)
+  })
 }
