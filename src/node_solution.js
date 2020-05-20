178f81ebc8b45e8a6abd60fcb9b36e7ad827b13d
ji <jkim0107@gmail.com>
2020-05-20 11:46:14 -0700

    working solution for part_2a

--- src/node_solution.js
+++ src/node_solution.js
@@ -4,34 +4,49 @@ const fs = require('fs');
 const contents = process.argv.splice(2);
 const data = JSON.parse(fs.readFileSync(contents[0], 'utf-8'));
 
-let currentCount = 0
-let currentTables = 0
-let parties = []
-let tables = []
+const partiesCount = data.parties.reduce((tot, curr) => {
+  return curr.count + tot
+}, 0)
 
-data['parties'].forEach(party => {
-  currentCount = party.count
-  while (currentCount > 0) {
-    parties.push(party.partyName)
-    currentCount--
-  }
-})
+const seatsCount = data.tables.reduce((tot, curr) => {
+  return curr.numberOfSeats + tot
+}, 0)
 
-data['tables'].forEach(table => {
-  currentTables = table.numberOfSeats
-  while (currentTables > 0) {
-    tables.push(table.tableName)
-    currentTables--
-  }
-})
-
-if (parties.length > tables.length) {
-  console.log(`We do not have enough seats!`)
-} else {
-  for (let i = 0; i < parties.length; i++) {
-    console.log(`${parties[i]} at ${tables[i]}`)
+let deductSeat = function(tableName, count) {
+  let table = data.tables.filter(t => {
+    return t.tableName === tableName
+  })
+  if (count > table.numberOfSeats) {
+    return false
+  } else {
+    table.numberOfSeats -= count
   }
 }
 
+let tableIndex = 0
 
-// console.log('data =', data);
+if (partiesCount > seatsCount) {
+  console.log(`We do not have enough seats!`)
+} else {
+  for (let i = 0; i < data.parties.length; i++) {
+    let party = data.parties[i]
+    if (party.hasOwnProperty('tableSeed')) {
+      while (party.count > 0) {
+        console.log(`${party.partyName} at ${party.tableSeed}`)
+        party.count--
+      }
+      deductSeat(party.tableSeed, party.count)
+    } else {
+      if (party.count === 1) {
+        i++
+        if (data.tables[tableIndex].numberOfSeats === 1) {
+          tableIndex++
+        }
+      }
+      console.log(`${party.partyName} at ${data.tables[tableIndex].tableName}`)
+      i--
+      party.count--
+      data.tables[tableIndex].numberOfSeats--
+    }
+  }
+}
